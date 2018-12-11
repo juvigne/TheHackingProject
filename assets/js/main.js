f300dc20beb2422e312ddd9a542dfc842b5512f9
juvigne <contactvigure@gmai.com>
2018-12-11 21:55:47 +0100

    add jquery and add popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
-setTimeout(function(){ 
-  if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")) {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); 
-  } 
-}, 10000);
+//document.ready => lance le code js lorsque la page(document) est entierment chargé
+$(document).ready(function() {
+  setTimeout(function(){
+    $( "#dialog" ).dialog();
+  }, 10000);
+} );
+
+//On ferme la popUp
+function close(){
+  $( "#dialog" ).dialog('close');
+}
+//On redirige vers une autre page
+function newTab(){
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+}
